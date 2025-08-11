3c10682132458334c67298172420ba92624ba1be
MD:sahariar Ahammed Emon <sahariargd28@gmail.com>
2025-08-11 16:30:13 +0600

    jsx and html uase

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import Header from "./assets/Header"
 import Login, {Profile, Setting, UserKey } from "./UserComponent"
 
 function App() {
+  const userName = "Sahariar Emon";
+  let x = 20;
+  let y = 50;
   return (
-    <div>
-    
-      <Header />
-      <Login />
-      <Profile />
-      <Setting />
-      <h1>{UserKey}</h1>
-    </div>
+    <>
+      <h1>{userName}</h1>
+      <h2>{2 + 4 * 4}</h2>
+      <h3>{x * y}</h3>
+      <button onClick={() => {
+        alert("Hello!")
+      }}>Click Me</button>
+    </>
   )
 }
 
